9b802bce21bd497842cd16d44315ee73b79c9d8c
Jake Leahy <jake@leahy.dev>
2021-12-25 11:43:17 +1100

    Wrapped decoder and encoder

--- src/opussum.nim
+++ src/opussum.nim
@@ -1,4 +1,3 @@
-
 {.passC: staticExec("pkg-config --cflags opus").}
 {.passL: staticExec("pkg-config --libs opus").}
 
@@ -10,18 +9,21 @@
 type
   OpusErrorCodes* = enum
     ## Various error codes that can be returned.
-    ##
-    ## .. Note:: While these are positive, opus returns negative codes so make sure to do conversion
-    BadArg = 11.cint
-    InternalError = 13.cint
-    Unimplemented = 15.cint
-    AllocFail = 17.cint
-    CannotOpen = 30.cint
-    TooLate = 31.cint
-    InvalidPicture = 32.cint
-    InvalidIcon = 33.cint
-    WriteFail = 34.cint
-    CloseFail = 35.cint
+    ## * **BadArg**: One or more invalid/out of range arguments
+    ## * **BufferTooSmall**: Not enough bytes allocated in the buffer.
+    ## * **InternalError**: An internal error was detected.
+    ## * **InvalidPacket**: The compressed data passed is corrupted.
+    ## * **Unimplemented**: Invalid/unsupported request number.
+    ## * **InvalidState**: An encoder or decoder structure is invalid or already freed.
+    ## * **AllocFail**: Memory allocation has failed
+    ## .. Note:: While these are positive, opus returns negative codes so make sure to do conversion (Or use checkRC_)
+    BadArg = 1
+    BufferTooSmall = 2
+    InternalError = 3
+    InvalidPacket = 4
+    Unimplemented = 5
+    InvalidState = 6
+    AllocFail = 7
 
   OpusApplicationModes* = enum
     ## * **Voip**: Best for most VoIP/videoconference applications where listening quality and intelligibility matter most
@@ -33,11 +35,20 @@ type
 
   OpusEncoderRaw* = object
     ## The C struct of OpusEncoder. It is recommended to use OpusEncoder_ procs instead since they handle memory
+  OpusDecoderRaw* = object
+    ## The C struct of OpusDecoder. It is recommended to use OpusDecoder_ procs instead since they handle memory
 
-  OpaqueObject*[T: object] = object
+  OpaqueOpusObject*[T: object] = object
     internal*: ptr T
-    
-  OpusEncoder* = OpaqueObject[OpusEncoderRaw]
+    frameSize*: int
+    channels*: int
+
+  PCMBytes* = object
+    len*: int
+    data*: ptr UncheckedArray[opusInt16]
+  
+  OpusEncoder* = OpaqueOpusObject[OpusEncoderRaw]
+  OpusDecoder* = OpaqueOpusObject[OpusDecoderRaw]
 
   OpusError* = object of CatchableError
 
@@ -51,7 +62,9 @@ type
 
   opusInt16* = cshort
   opusInt32* = cint
-  
+
+  OpusFrame* = distinct cstring
+
 
 template checkRC*(call: untyped) =
   ## Checks the return value of a function and throws error if < 0.
@@ -61,7 +74,7 @@ template checkRC*(call: untyped) =
       {.warning[HoleEnumConv]: off.}
     let error = OpusErrorCodes(abs(res))
     raise (ref OpusError)(msg: $error)
-    
+  
 let
   opusVersion* {.opusHead, importc: "API_VERSION".}: cint
     ##  API version for this opus headers. Can be used to check for features at compile time  
@@ -85,15 +98,14 @@ proc init*(st: ptr OpusEncoderRaw, fs: opusInt32, channels, application: cint): 
   ## * **fs**: Sampling rate of input signal (Hz). This must be one of 8000, 12000, 16000, 24000, or 48000
   ## * **channels**: Number of channels in input signal
   ## * **application**: Coding mode. See _OpusApplicationModes
-
-
   
-proc encode*(st: ptr OpusEncoderRaw, pcm: ptr opusInt16, frameSize: cint, data: cstring, max_data_bytes: opusInt32): opusInt32 {.importc: "opus_encode"}
+proc encode*(st: ptr OpusEncoderRaw, data: ptr opusInt16, frameSize: cint, outData: cstring, max_data_bytes: opusInt32): opusInt32 {.importc: "opus_encode"}
   ## Encodes an opus frame
   ## * **st**: Encoder state
-  ## * **pcm**: Input signal (interleaved if 2 channels). Length is (frame_size * channels * sizeof(opus_int16))
-  ## * **data**: Output payload. This must contain storage for at least max_data_bytes
-  ## * **max_data_bytes**: Size of the allocated memory for the output payload. This may be used to impose an upper limit on the instant bitrate, but should not be used as the only bitrate control. Use setBitrate_ to control the bitrate
+  ## * **data**: Input signal (interleaved if 2 channels). Length is (frame_size * channels * sizeof(opus_int16))
+  ## * **frameSize**: Number of samples per channel in the input signal. This must be an Opus frame size
+  ## * **outData**: Output payload. This must contain storage for at least max_data_bytes
+  ## * **max_data_bytes**: Size of the allocated memory for the output payload (4000 is recommended). This may be used to impose an upper limit on the instant bitrate, but should not be used as the only bitrate control. Use setBitrate_ to control the bitrate.
   ## * **returns**: Length of the encoded bytes on success or error code on failure
 proc opusCreateEncoder*(fs: opusInt32, channels, application: cint, error: ptr cint): ptr OpusEncoderRaw {.importc: "opus_encoder_create".}
   ## Allocates and initialises an encoder state
@@ -106,18 +118,133 @@ proc opusCreateEncoder*(fs: opusInt32, channels, application: cint, error: ptr c
 proc destroy*(st: ptr OpusEncoderRaw) {.importc: "opus_encoder_destroy".}
   ## Frees an OpusEncoderRaw_ allocated by encoderCreate_
 
+#
+# Decoder
+#
+
+proc getDecoderSize*(channels: cint): cint {.importc: "opus_decoder_get_size".}
+  ## Gets the size of an OpusDecoderRaw_ structu
+  ## * **channels**: Number of channels. This must be 1 or 2
+
+proc opusCreateDecoder*(fs: opusInt32, channels: cint, error: ptr cint): ptr OpusDecoderRaw {.importc: "opus_decoder_create".}
+  
+proc destroy*(str: ptr OpusDecoderRaw) {.importc: "opus_decoder_destroy".}
+
+proc decode*(st: ptr OpusDecoderRaw, data: cstring, len: opusInt32, outData: ptr opusInt16, frame_size, decode_fec: cint): cint {.importc: "opus_decode".}
+
 {.pop.}
 
-proc createEncoder*(fs: int32, channels: range[1..2], application: OpusApplicationModes): OpusEncoder =
+template makeDestructor(kind: untyped) =
+  proc `=destroy`*(obj: var kind) =
+    ## Cleans up an OpaqueObject_ by destroying the internal pointer
+    if obj.internal != nil:
+      destroy obj.internal
+      obj.internal = nil
+
+# Generic destructors don't work for some reason so I must manually define
+makeDestructor(OpusDecoder)
+makeDestructor(OpusEncoder)
+
+proc `=destroy`*(pcm: var PCMBytes) =
+  if pcm.data != nil:
+    freeShared pcm.data
+    pcm.data = nil
+
+template checkSampleRate(sampleRate: int32) =
+  assert sampleRate in allowedSamplingRates, "sampling must be one of 8000, 12000, 16000, 24000, or 48000"
+
+proc createEncoder*(sampleRate: int32, channels: range[1..2], frameSize: int, application: OpusApplicationModes): OpusEncoder =
   ## Creates an encoder (this does not need to be destroyed manually)
-  assert fs in allowedSamplingRates, "sampling must be one of 8000, 12000, 16000, 24000, or 48000"
+  checkSampleRate sampleRate
   var error: cint
-  # result.internal = opusCreateEncoder(fs.opusInt32, channels.cint, application.ord.cint, addr error)
+  result.internal = opusCreateEncoder(sampleRate.opusInt32, channels.cint, application.ord.cint, addr error)
+  result.frameSize = frameSize
+  result.channels = channels
   checkRC error
+
+proc createDecoder*(sampleRate: int32, channels: range[1..2], frameSize: int): OpusDecoder =
+  checkSampleRate sampleRate
+  var error: cint
+  result.internal = opusCreateDecoder(sampleRate, channels, addr error)
+  result.frameSize = frameSize
+  result.channels = channels
+
+#[
+     {
+      int i;
+      unsigned char pcm_bytes[MAX_FRAME_SIZE*CHANNELS*2];
+      int frame_size;
+
+      /* Read a 16 bits/sample audio frame. */
+      fread(pcm_bytes, sizeof(short)*CHANNELS, FRAME_SIZE, fin);
+      if (feof(fin))
+         break;
+      /* Convert from little-endian ordering. */
+      for (i=0;i<CHANNELS*FRAME_SIZE;i++)
+         in[i]=pcm_bytes[2*i+1]<<8|pcm_bytes[2*i];
+]#
+
+proc packetSize*[T](obj: OpaqueOpusObject[T]): int {.inline.} =
+  result = obj.frameSize * obj.channels
+
+proc toPCMBytes(data: sink string, frameSize, channels: int): PCMBytes =
+  ## Converts a string to pcm bytes
+  let size = frameSize * channels * 2
+  echo size
+  result.len = size
+  result.data = cast[ptr UnCheckedArray[opusInt16]](createShared(opusInt16, size))
+  # Convert to little endian int16 
+  for i in 0..<(channels * frameSize):
+    result.data[i] = cast[opusInt16]((data[2 * i + 1].ord shl 8) or data[2 * i].ord)
+
+proc toPCMBytes*[T](data: sink string, opus: OpaqueOpusObject[T]): PCMBytes =
+  ## Like the other `toPCMBytes` except it uses the settings from an OpusEncoder_ or OpusDecoder_
+  result = data.toPCMBytes(opus.frameSize, opus.channels)
+    
+proc encode*(encoder: OpusEncoder, data: PCMBytes, size: int): OpusFrame =
+  ## Encodes an opus frame
+  # runnableExamples:
+    # import std/streams
+    # let
+      # rawData = newFileStream("tests/test.raw", fmRead)
+      # encoder = createEncoder(48000, 2, 960, Voip)
+      # pcmBytes = rawData.readStr(1920 * 2).toPcmBytes(encoder)
+    # let encoded = encoder.encode(pcmBytes, 1920)
+     # 
+  assert encoder.internal != nil, "Encoder has been destroyed"
+  # Allocate needed buffers
+  var outData = cast[cstring](createShared(char, size))
   
-proc `=destroy`*[T](obj: var OpaqueObject[T]) =
-  ## Cleans up an OpaqueObject_ by destroying the internal pointer
-  if obj != nil and obj.internal != nil:
-    destroy obj.internal
-    obj.internal = nil
-    obj = nil
+  let length = encoder.internal.encode(
+    cast[ptr opusInt16](data.data),
+    encoder.frameSize.cint,
+    outData,
+    size.cint
+  )
+  checkRC length
+  
+  # Move bytes to result
+  # TODO: benchmark if it would be faster to do setLen and then move cstring to result
+  result = OpusFrame(newString length)
+  for i in 0..<length:
+    result.cstring[i] = outData[i]
+
+proc decode*(decoder: OpusDecoder, encoded: OpusFrame, errorCorrection: bool = false): PCMBytes =
+  ## Decodes an opus frame
+  let packetSize = decoder.packetSize
+  result.data = cast[ptr UncheckedArray[opusInt16]](createShared(opusInt16, packetSize))
+  let length = decoder.internal.decode(
+    encoded.cstring, 
+    encoded.cstring.len.opusInt32,
+    cast[ptr opusInt16](result.data),
+    cint(packetSize * 2),
+    cast[cint](errorCorrection)
+    )
+  checkRC length
+  result.len = length
+  
+  
+proc destroy*[T](obj: OpaqueOpusObject[T]) {.inline.} =
+  ## Calls the `destroy` proc that is relevant for the internal pointer
+  destroy obj.internal
+  obj.internal = nil
--- tests/testApi.nim
+++ tests/testApi.nim
@@ -1,7 +1,35 @@
-
 import unittest
-
 import opussum
-var encoder: OpusEncoder
-test "Create encoder":
-  encoder = createEncoder(48000, 2, Voip)
+import std/streams
+
+#
+# Test data comes from joystock
+# Music by Joystock - https://www.joystock.org
+#
+
+let rawData = newFileStream("tests/test.raw", fmRead)
+
+var canEncode: bool
+
+suite "Encoder":
+  var encoder: OpusEncoder
+  test "Create encoder":
+    encoder = createEncoder(48000, 2, 960, Voip)
+
+  test "Encode data":
+    const size = 1920
+    discard encoder.encode(rawData.readStr(size * 2).toPCMBytes(encoder), size)
+    canEncode = true
+    
+suite "Decoder":
+  var decoder: OpusDecoder
+  test "Create decoder":
+    decoder = createDecoder(48000, 2, 960)
+
+  test "Decode data":
+    check canEncode
+    let encoder = createEncoder(48000, 2, 960, Voip)
+    let pcmBytes = rawData.readStr(1920 * 2)
+    let encodedData = encoder.encode(pcmBytes.toPCMBytes(encoder), 1920)
+
+GC_fullcollect()
